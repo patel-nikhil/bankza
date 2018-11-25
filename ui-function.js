809a1df73e74011bfca6ea3b97e43ed8fecab3ac
David Hobson <davidhobson96@gmail.com>
2018-11-24 20:22:55 -0500

    Create basic functions for the user interface and transitions between states.

---
+++ ui-function.js
@@ -0,0 +1,58 @@
+let bankUI = $("#bank-ui");
+let state = 0;
+
+$(".pin-pad-screen").toggle();
+$(".main-menu-screen").toggle();
+$(".menu-buttons-set").toggle();
+
+
+function toggleStateDisplay(screen){
+	screen.fadeToggle(1000);
+}
+
+bankUI.click(function(){
+	if(state == 0){
+		state = 1;
+		toggleStateDisplay($(".start-screen"));
+		setTimeout(function(){
+			toggleStateDisplay($(".pin-pad-screen"));	
+		}, 1000);
+
+	}
+});
+
+$(".pin-enter").click(function(){
+	if(state == 1){
+		state = 2;
+		toggleStateDisplay($(".pin-pad-screen"));
+		setTimeout(function(){
+			toggleStateDisplay($(".main-menu-screen"));	
+			toggleStateDisplay($(".menu-buttons-set"));	
+		}, 1000);
+
+	}
+});
+
+
+
+let pinNumbers = $(".pin-num");
+let entry = "";
+
+pinNumbers.each(function(index, btn){
+	$(btn).click(function(){
+		if($(".pin-input").val().length < 4){
+			entry += (index + 1) % 10
+			$(".pin-input").val(entry);	
+		}
+	});
+});
+
+
+$(".pin-delete").click(function(){
+		
+		let numArray = entry.split("");
+		numArray.pop();
+		entry = numArray.join("");
+
+		$(".pin-input").val(entry);
+});
