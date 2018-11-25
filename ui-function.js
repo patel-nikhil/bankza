1a10017e30a8cf131db3b3b2147487db9b55798a
np123 <patenikh2@gmail.com>
2018-11-25 00:17:14 -0500

    Added account entry screen and input clear button

--- ui-function.js
+++ ui-function.js
@@ -1,6 +1,7 @@
 let bankUI = $("#bank-ui");
 let state = 0;
 
+$(".account-entry-screen").toggle();
 $(".pin-pad-screen").toggle();
 $(".main-menu-screen").toggle();
 $(".menu-buttons-set").toggle();
@@ -15,21 +16,32 @@ bankUI.click(function(){
 		state = 1;
 		toggleStateDisplay($(".start-screen"));
 		setTimeout(function(){
-			toggleStateDisplay($(".pin-pad-screen"));	
+			toggleStateDisplay($(".account-entry-screen"));	
 		}, 1000);
 
 	}
 });
 
-$(".pin-enter").click(function(){
+$(".account-enter").click(function(){
 	if(state == 1){
 		state = 2;
+		$(".pin-input").val("");
+		entry = "";
+		toggleStateDisplay($(".account-entry-screen"));
+		setTimeout(function(){
+			toggleStateDisplay($(".pin-pad-screen"));	
+		}, 1000);
+	}
+});
+
+$(".pin-enter").click(function(){
+	if(state == 2){
+		state = 3;
 		toggleStateDisplay($(".pin-pad-screen"));
 		setTimeout(function(){
-			toggleStateDisplay($(".main-menu-screen"));	
-			toggleStateDisplay($(".menu-buttons-set"));	
+			toggleStateDisplay($(".main-menu-screen"));
+			toggleStateDisplay($(".menu-buttons-set"));
 		}, 1000);
-
 	}
 });
 
@@ -43,6 +55,9 @@ pinNumbers.each(function(index, btn){
 		if($(".pin-input").val().length < 4){
 			entry += (index + 1) % 10
 			$(".pin-input").val(entry);	
+		} else if ($(".pin-input").val().length < 7 && state == 1){
+			entry += (index + 1) % 10
+			$(".pin-input").val(entry);
 		}
 	});
 });
@@ -55,4 +70,10 @@ $(".pin-delete").click(function(){
 		entry = numArray.join("");
 
 		$(".pin-input").val(entry);
+});
+
+$(".pin-clear").click(function(){
+		
+	entry = "";
+	$(".pin-input").val(entry);
 });
