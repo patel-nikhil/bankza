829361379d3522ab7c5ca1f26152c26a1029d783
David Hobson <davidhobson96@gmail.com>
2018-11-28 08:52:17 -0500

    Modify accounts for testing purposes."

--- accounts.js
+++ accounts.js
@@ -1,25 +1,23 @@
 var activeAccount;
 var activeAccountType;
-var accountName = 0;
-var total = 1;
 
 var account1 = {
-    number: "1234567890904321",
-    balance: [[chequing, 100.00], [savings, 50.00]],
+    number: "1",
+    balance: [["chequing", 100.00], ["savings", 50.00]],
     name: "Adam Geneva",
-    pin: "1234"
+    pin: "0000"
 }
 
 var account2 = {
     number: "1234567890904322",
-    balance: [[chequing, 200.00], [savings, 50.00]],
+    balance: [["chequing", 200.00], ["savings", 50.00]],
     name: "Inspectah Malicious",
-    pin: "1234"
+    pin: "5678"
 }
 
 var accountF = {
     number: "523456789090432",
-    balance: [[chequing, 300.00], [savings, 50.00]],
+    balance: [["chequing", 300.00], ["savings", 50.00]],
     name: "King Mickey",
-    pin: "1234"
+    pin: "0000"
 }
